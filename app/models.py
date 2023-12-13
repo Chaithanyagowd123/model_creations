af9e2214fc4d577dc0e2ad75d9ac1aa5e5982cf6
Chaithanyagowd@1234 <kaithepallichaithanya@gmail.com>
2023-12-13 19:27:59 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Webpage(models.Model):
     topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     url=models.URLField()
+    email=models.EmailField(default='india@gmail.com')
 
 
 
@@ -24,7 +25,6 @@ class AccessRecord(models.Model):
     name=models.ForeignKey(Webpage,on_delete=models.CASCADE)
     date=models.DateField()
     author=models.CharField(max_length=100)
-
 
     def __str__(self):
         return self.author
--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+# 
+def display_topics(request):
+
+    QLTO=Topic.objects.all()
+    d={'topics':QLTO}
+
+
+    return render(request,'display_topics.html',d)
+
+
+
+def display_webpages(request):
+    QLWO=Webpage.objects.all()
+    d={'webpages':QLWO}
+
+    return render(request,'display_webpages.html',d)
+
+
+def display_records(request):
+    QLAO=AccessRecord.objects.all()
+    d={'AccessRecords':QLAO}
+
+    return render(request,'display_records.html',d)
